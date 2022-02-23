cb4f362061056f8d8903afce72c3fe38f67de904
zhaubin <100251439+zhaubin@users.noreply.github.com>
2022-02-23 11:51:37 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,207 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 4 4 . . . . . . . 
+        . . . . . . 4 5 5 4 . . . . . . 
+        . . . . . . 2 5 5 2 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, ship, 0, -150)
+})
+let projectile2: Sprite = null
+let projectile: Sprite = null
+let ship: Sprite = null
+ship = sprites.create(img`
+    . . . . . f f f f . . . . . 
+    . . . f f 5 5 5 5 f f . . . 
+    . . f 5 5 5 5 5 5 5 5 f . . 
+    . f 5 5 5 5 5 5 5 5 5 5 f . 
+    . f 5 5 5 d b b d 5 5 5 f . 
+    f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+    f 5 5 c c 4 4 4 4 c c 5 5 f 
+    f b b f b f 4 4 f b f b b f 
+    f b b 4 1 f d d f 1 4 b b f 
+    . f b f d d d d d d f b f . 
+    . f e f e 4 4 4 4 e f e f . 
+    . e 4 f 6 9 9 9 9 6 f 4 e . 
+    . 4 d c 9 9 9 9 9 9 c d 4 . 
+    . 4 f b 3 b 3 b 3 b b f 4 . 
+    . . f f 3 b 3 b 3 3 f f . . 
+    . . . . f f b b f f . . . . 
+    `, SpriteKind.Player)
+ship.bottom = 120
+controller.moveSprite(ship)
+info.setLife(3)
+effects.starField.startScreenEffect()
+ship.setStayInScreen(true)
+let asteroids = [
+img`
+    ..........bbbbbb................
+    .......bbb444444bb..............
+    .....2244444ddd444b.............
+    ....244444444dddd44e............
+    ...244444444444ddd4be...........
+    ..244444444444444d44be..........
+    .2b444444444444444d4be..........
+    .2b44444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bb4b4444444444444444bbe........
+    2bb4444444444444444444be........
+    2bb44444444444444444444e........
+    2bbb444bbb4444444444444e........
+    22bbb444bb4bb444444444be........
+    .2bbbbb44bbbb44444444bbe........
+    .22bbbbbbbb44bbb444444bbe.......
+    ..eeebbbbbbb44bbb444444be.......
+    ...eeeeebbbbbbbb44b4444be.......
+    .....eeeeee222bb44bbb4bbe.......
+    .......eeeee222bb44bbbbee.......
+    ............e222bbbbbbbec.......
+    ..............ee2bbbbeebdb......
+    .................eeeeecdddb.....
+    .......................cd11bbbb.
+    ........................cd111dbb
+    .........................b11111c
+    .........................c11dd1c
+    .........................cd1dbc.
+    .........................cb11c..
+    ..........................ccc...
+    ................................
+    `,
+img`
+    ......................bbb.......
+    ....................bb333b......
+    .................bbb333d33b.....
+    ................bb333333d3a.....
+    ..............bb33332eeeedba....
+    ............bbb333323eee2e3a....
+    ..........bbd333333e22222ed3a...
+    .......bbbdd3333333e22222edda...
+    ......bb3d333333333be222eb3d3a..
+    ...bbb3dd33333333333beeeb33d3a..
+    ..b3ddd33333333333333333333dda..
+    bbddd3333333333333333333333dd3a.
+    b33dddddd3333333333333333333d3a.
+    bb3333333ddddd33333333333333dda.
+    bbbbbbb333dd33dddddddddd3333ddba
+    b55553bbbbbb3333dd33333ddd33dd3a
+    b555555555553bbbbbbbb33333dddd3a
+    bd555555555555555dddbaaaaab3d3ba
+    bb55555555555555555dddddddbb33ba
+    b3bb35555555555d5555d55dddddbbba
+    b33333bbb355dd55555d555ddddddbba
+    b5555d333333bbb35dddddd55ddddbba
+    b5d555dd5553333bbbbb3ddddddddb3a
+    b5d555555555555dd3333bbbbbb3db3a
+    bd5d555555d55555dd555ddbbbbbbb3a
+    bbb55dd555555555555555ddddddbb3a
+    ...bbbbdd555ddd5555ddddddddddb3a
+    .......bbbb555555d5ddd5ddddddb3a
+    ...........bbbb55555555555dd533a
+    ...............bbbbddd5d55d5b3ba
+    ...................bbbbddddb3ba.
+    .......................bbbaaaa..
+    `,
+img`
+    .............beebbbb............
+    ............eebbbb4bb...........
+    ............eb344bb4bb..........
+    ............e44334bb4bb.........
+    ............eb433344b4be........
+    ............4eb43344444be.......
+    ...........bd4eb43333344bb......
+    ..........b455d4443333444bb.....
+    ..........4d5555d444333444bb....
+    .........4555555dd4b4443444be...
+    ........bd5555d555d4bb444444ee..
+    ........b55ddd665555bb4b44444ee.
+    .......bd5555677655554ebb44444eb
+    .......43222558855555d4eeb44b4ee
+    ......b422332ddd555222d4eebbb4be
+    ......be22232ed55522332db4ebbbbe
+    .....bde22222e555e22232edd4bbbbe
+    .....b52e222e3555e22222eddd4ebee
+    ....bd552eee355552e222e355544eee
+    ....665dd5555555552eee355dd4deee
+    ...6776555555555555555551554d4ee
+    ...4885222555dddd6655551544d4eee
+    ..b45522332555dd677611d444ddeee.
+    ..4d5222232e55555881d44ddd4eee..
+    .bdd5e22222e555115114d54d4ee....
+    .b55d2e222e351144d1d55eeee......
+    bd5ddd2eee3d444555dd4e..........
+    b555115dddd55d544eede...........
+    4511d444d5544ee...4de...........
+    41d4555d4ee........44...........
+    41554eede.......................
+    44ee...4e.......................
+    `,
+img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `,
+img`
+    ...........ccccc66666...........
+    ........ccc4444444444666........
+    ......cc444444444bb4444466......
+    .....cb4444bb4444b5b444444b.....
+    ....eb4444b5b44444b44444444b....
+    ...ebb44444b4444444444b444446...
+    ..eb6bb444444444bb444b5b444446..
+    ..e6bb5b44444444b5b444b44bb44e..
+    .e66b4b4444444444b4444444b5b44e.
+    .e6bb444444444444444444444bb44e.
+    eb66b44444bb444444444444444444be
+    eb66bb444b5b44444444bb44444444be
+    fb666b444bb444444444b5b4444444bf
+    fcb666b44444444444444bb444444bcf
+    .fbb6666b44444444444444444444bf.
+    .efbb66666bb4444444444444444bfe.
+    .86fcbb66666bbb44444444444bcc688
+    8772effcbbbbbbbbbbbbbbbbcfc22778
+    87722222cccccccccccccccc22226678
+    f866622222222222222222222276686f
+    fef866677766667777776667777fffef
+    fbff877768f86777777666776fffffbf
+    fbeffeefffeff7766688effeeeefeb6f
+    f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+    f66ddfffffeeeffeffeeeeeffeedb46e
+    .c66ddd4effffffeeeeeffff4ddb46e.
+    .fc6b4dddddddddddddddddddb444ee.
+    ..ff6bb444444444444444444444ee..
+    ....ffbbbb4444444444444444ee....
+    ......ffebbbbbb44444444eee......
+    .........fffffffcccccee.........
+    ................................
+    `
+]
+game.onUpdateInterval(500, function () {
+    projectile2 = sprites.createProjectileFromSide(asteroids[randint(0, 4)], 0, 50)
+    projectile2.setKind(SpriteKind.Enemy)
+    projectile2.x = randint(10, 150)
+})
